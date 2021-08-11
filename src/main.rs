9bc3f4ca0d2fa240cd9d4da03be24455c6905498
Andrey Kruglov <kruglov.it@gmail.com>
2021-08-12 02:42:29 +0300

    fixed by downgrade actix library

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,13 @@
 use actix::{Actor, StreamHandler};
 use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use actix_web_actors::ws;
-use std::time::Instant;
-
-async fn ws_index(r: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    println!("{:?}", r);
-    let res = ws::start(MyWs::new(), &r, stream);
-    println!("{:?}", res);
-    res
-}
 
 /// Define HTTP actor
-struct MyWs {
-    hb: Instant,
-}
+struct MyWs;
 
 impl Actor for MyWs {
     type Context = ws::WebsocketContext<Self>;
 
-    fn started(&mut self, ctx: &mut Self::Context) {
-        self.hb(ctx);
-    }
-}
-
-impl MyWs {
-    fn new() -> Self {
-        Self { hb: Instant::now() }
-    }
 }
 
 /// Handler for ws::Message message
@@ -46,8 +27,7 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
 }
 
 async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    let resp = ws::start(MyWs::new(), &req, stream);
-    println!("{:?}", resp);
+    let resp = ws::start(MyWs {}, &req, stream);
     resp
 }
 
